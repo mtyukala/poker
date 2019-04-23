33827ad9d11d8442014cf39e3425e22624311242
mtyukala <mkhululi.tyukala@gmail.com>
2019-04-23 20:56:06 +0200

    Added Application.java and PokerGame.java classes

---
+++ poker.test/src/main/java/com/poker/poker/test/Application.java
@@ -0,0 +1,15 @@
+package com.poker.poker.test;
+
+/**
+ *
+ * @author mt
+ */
+public class Application {
+    public static void main(String[] args) {
+        PokerGame game = new PokerGame();
+        game.play();
+        game.draw();
+        game.score();
+    }
+    
+}
